49ee0f788cc0cef2491b959972478d088a064de8
Diego Gutierrez <gutierrezdiegofrancesco@gmail.com>
2026-04-20 13:26:04 -0500

    fix2

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -6,26 +6,22 @@ export const AppProvider = ({ children }) => {
   const [userRole, setUserRole] = useState(null);
   const [cart, setCart] = useState([]);
 
-  // Añadir al carrito respetando el límite de stock
   const addToCart = (moto) => {
     const itemExiste = cart.find(item => item.id === moto.id);
     
     if (itemExiste) {
-      // Solo suma si la cantidad actual es menor que el stock
       if (itemExiste.cantidad < moto.stock) {
         setCart(cart.map(item => 
           item.id === moto.id ? { ...item, cantidad: item.cantidad + 1 } : item
         ));
       }
     } else {
-      // Si es nuevo y hay stock mayor a 0, lo añade con cantidad 1
       if (moto.stock > 0) {
         setCart([...cart, { ...moto, cantidad: 1 }]);
       }
     }
   };
 
-  // Restar del carrito o eliminar
   const removeFromCart = (motoId) => {
     const itemExiste = cart.find(item => item.id === motoId);
     if (itemExiste.cantidad > 1) {
